d8be0b99a023ab9e2e5e7954474847a3528502fb
Jack Clancy <jack.clancy93@gmail.com>
2015-10-10 19:50:06 -0700

    added home.php file

---
+++ resources/views/home.php
@@ -0,0 +1,64 @@
+<!doctype html>
+
+<html>
+<head>
+  <title>Globely Home</title>
+  <link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/3.0.2/normalize.css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="/css/app.css"></link>
+</head>
+<body>
+  <header>
+    <nav>
+      <a href="#">About</a>
+      <a href="#">Sign In</a>
+      <a href="#" class="with-border">Apply</a>
+    </nav>
+    <div class="logo">Globely</div>
+  </header>
+  <section class="slider">
+    <div class="content">
+      <h1>The Global Way to Buy and Sell</h1>
+      <h4>Welcome to the new way to bring goods to market</h4>
+      <button class="blue">Log In</button>
+      <button class="teal">Apply</button>
+    </div>
+  </section>
+  <section>
+    <div class="content">
+      <h1>H1 - Main</h1>
+      <h2>H2 - Sub Heading</h2>
+      <h3>H2 - Subbest Heading</h3>
+      <p>Paragraph text - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </section>
+  <section>
+    <div class="content">
+      <h2>Buttons</h2>
+      <button class="green">Over Here!</button>
+      <button class="teal">Click Me!</button>
+      <button class="blue">No Me!</button>
+    </div>
+  </section>
+  <section>
+    <div class="content">
+      <h2>Input Boxes</h2>
+      <label for="green">Green</label>
+      <input id="gren" class="green" type="text" placeholder="I'm a textbox!">
+      <label for="teal">Teal</label>
+      <input id="teal" class="teal" type="text" placeholder="Please write in me!">
+      <label for="blue">Blue</label>
+      <input id="blue" class="blue" type="text" placeholder="Me too!">
+    </div>
+  </section>
+  <footer class="footer">
+      2015 Globely 
+  </footer>
+</body>
+</html>
