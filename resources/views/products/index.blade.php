e79b35450b149e14a766cf72b99179a27bba33d6
Jack Clancy <jack.clancy93@gmail.com>
2015-11-04 15:10:48 -0800

    Added some middleware

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,74 +6,81 @@
       <h1>Products</h1>
     </div>
     </section>
-    <section>
-     <div class="card col-md-3">
-        <div class="row">
-          <div class="col-md-6">
-            <img class="product-image" src="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">
-          </div>
-          <div class="col-md-6">
-            <h2 class="product-title">Product 1</h2>
-            <p class="product-brand">by Jack</p>
-            <p class="product-desc">a cool product</p>
-            <p class="product-price">$100 <span>Rating: 5/5</span></p>
-            <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
-          </div>
-        </div>
-     </div>
-
-          <div class="card col-md-3">
-        <div class="row">
-          <div class="col-md-6">
-            <img class="product-image" src="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">
-          </div>
-          <div class="col-md-6">
-            <h2 class="product-title">Product 1</h2>
-            <p class="product-brand">by Jack</p>
-            <p class="product-desc">a cool product</p>
-            <p class="product-price">$100 <span>Rating: 5/5</span></p>
-            <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
-          </div>
-        </div>
-     </div>
-                    <div class="col-md-6">
-                            <div class="content">
-                               <div class="card"> 
+    <section class="container-fluid">
+                    <div class="row">
+                               <div class="card col-md-4 col-md-offset-1"> 
                                     <div class="header text-center">
-                                        <h5 class="title">Product 1</h5>
+                                        <h2 class="title">Product 1</h2>
                                     </div>
-                                    <div class="content">
-                                    <h2 class="product-title">Product 1</h2>
+                                    <div class="row">
+                                    <div class="col-md-6">
+                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
+                                    </div>
+                                    <div class="col-md-6 product-info">
                                     <p class="product-brand">by Jack</p>
                                     <p class="product-desc">a cool product</p>
                                     <p class="product-price">$100 <span>Rating: 5/5</span></p>
                                     <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
+                                    <button class="green">Buy Now!</button>
+                                    </div>
                                     </div>
                                 </div>
-                                <div class="card"> 
+                                <div class="col-md-1"></div>
+                               <div class="card col-md-4 col-md-offset-1"> 
                                     <div class="header text-center">
-                                        <h5 class="title">Mall Condition</h5>
+                                        <h2 class="title">Product 1</h2>
                                     </div>
-                                    <div class="content">
-                                       <p>Paint the mall please</p>
-                                       <p>Need better restrooms</p>
-                                       <p>Hi, I am so glad you ask. First lets get a decent mall sign for Candler Rd. The existing one is very old with missing letters and ugly. There needs to be an up to date look and please include the new name to reflect a fresh image for this mall</p>
-                                       <p>Need Air conditioning outside the stores, those glass panels bring in too much heat into the mall during the summer</p>
+                                    <div class="row">
+                                    <div class="col-md-6">
+                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
                                     </div>
-                                </div>
-                                <div class="card"> 
-                                    <div class="header text-center">
-                                        <h5 class="title">Random</h5>
+                                    <div class="col-md-6 product-info">
+                                    <p class="product-brand">by Jack</p>
+                                    <p class="product-desc">a cool product</p>
+                                    <p class="product-price">$100 <span>Rating: 5/5</span></p>
+                                    <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
+                                    <button class="green">Buy Now!</button>
                                     </div>
-                                    <div class="content">
-                                       <p>South Dekalb has become the new Avondale mall; it needs some life in it. Less urban wear (wigs/weavs) can be found on every corner. Cleanliness and brighter, more attractive stores</p>
-                                       <p>It needs a total revamping. When we are forced to go to other counties to spend our money, the taxes remain in those counties. Look how nice other malls are---help South Dekalb, I can't remmember the last time I was there. I travel to Camp Creek, Cumberland, and Lenox Square</p>
-                                       <p>I travel to Camp Creek, Cumberland or Lenox Square  for better restaurants</p>
                                     </div>
-                                </div>
-                            </div>
                         </div>
-                    </div>                        
+                    </div>  
+                     <div class="row">
+                               <div class="card col-md-4 col-md-offset-1"> 
+                                    <div class="header text-center">
+                                        <h2 class="title">Product 1</h2>
+                                    </div>
+                                    <div class="row">
+                                    <div class="col-md-6">
+                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
+                                    </div>
+                                    <div class="col-md-6 product-info">
+                                    <p class="product-brand">by Jack</p>
+                                    <p class="product-desc">a cool product</p>
+                                    <p class="product-price">$100 <span>Rating: 5/5</span></p>
+                                    <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
+                                    <button class="green">Buy Now!</button>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-1"></div>
+                               <div class="card col-md-4 col-md-offset-1"> 
+                                    <div class="header text-center">
+                                        <h2 class="title">Product 1</h2>
+                                    </div>
+                                    <div class="row">
+                                    <div class="col-md-6">
+                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
+                                    </div>
+                                    <div class="col-md-6 product-info">
+                                    <p class="product-brand">by Jack</p>
+                                    <p class="product-desc">a cool product</p>
+                                    <p class="product-price">$100 <span>Rating: 5/5</span></p>
+                                    <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
+                                    <button class="green">Buy Now!</button>
+                                    </div>
+                                    </div>
+                        </div>
+                    </div>                       
                 </div>     
             </div>    
     </section>
