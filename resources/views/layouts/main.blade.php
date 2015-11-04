e3db9a875c3a58a7ae4e0ead76845a2503add4e2
Jack Clancy <jack.clancy93@gmail.com>
2015-11-04 00:05:13 -0800

    Styling product component

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,8 @@
 <head>
   <title>@yield('title')</title>
   <link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/3.0.2/normalize.css">
+  <link rel="stylesheet" type="text/css" href="  https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="/css/app.css"></link>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,18 +7,25 @@
     </div>
   </section>
     <section>
-      <div class="product">
-        <h2 class="product-title">Product 1</h2>
-        <p class="product-image">IMAGE GOES HERE</p>
-        <p class="product-brand">by Jack</p>
-        <p class="product-desc">a cool product</p>
-        <p class="product-price">$100 <span>Rating: 5/5</span></p>
-        <p class="product-rating">Rating: 5/5</p>
-      </div>
+     <div class="card col-md-3">
+        <div class="row">
+          <div class="col-md-6">
+            <img class="product-image" src="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">
+          </div>
+          <div class="col-md-6">
+            <h2 class="product-title">Product 1</h2>
+            <p class="product-brand">by Jack</p>
+            <p class="product-desc">a cool product</p>
+            <p class="product-price">$100 <span>Rating: 5/5</span></p>
+            <p class="product-rating"><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
+          </div>
+        </div>
+     <div>
     </section>
 
   @foreach ($products as $product)
     <section>
+      <div class="card">
       <div class="product">
         <h2>{{ $product->name }}</h2>
         <p>ID: {{ $product->id }}</p>
@@ -26,6 +33,7 @@
         <p>{{ $product->description }}</p>
         <p>Price: {{ $product->price }}</p>
         <p>Rating: {{ $product->rating }}/5</p>
+      </div>
       </div>
     </section>
   @endforeach
