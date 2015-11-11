086f11f9a51f289304df4a571c7e9a308a4e0ad9
Harvey <SnowZ@hiroshiryoumatoMacBook-Pro.local>
2015-11-11 00:06:25 -0800

    ?

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,2 +1,19 @@
-CATCHING UP!!!
-learing laravel
+<div class="footer">
+  <foot>
+  	<a href="http://localhost:8000">Home |</a>
+    <a href="/how-it-works"> About |</a>
+    <a href="/products"> Products |</a>
+    <a href="/users"> Users |</a>
+  </foot>
+   <a href="/">
+    <img src="/images/globely_white_logo.png" class="logo" alt="Globley Logo">
+  </a>
+</div>
+
+
+
+
+
+
+
+
