33023b8c349622b15d22d531a07f571066416cbc
hlenner <lenner@usc.edu>
2015-11-15 18:15:09 -0800

    Jack changes to add product styling:

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,96 +11,29 @@
     </div>
     </section>
     <section class="container-fluid">
-                    <div class="row">
-                               <div class="card col-md-4 col-md-offset-1"> 
-                                    <div class="header text-center">
-                                        <h2 class="title">Product 1</h2>
-                                    </div>
-                                    <div class="row">
-                                    <div class="col-md-6">
-                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
-                                    </div>
-                                    <div class="col-md-6 product-info">
-                                    <p class="product-brand">by Jack</p>
-                                    <p class="product-desc">a cool product</p>
-                                    <p class="product-price">$100 <span>Rating: 5/5</span></p>
-                                    <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
-                                    <button class="green">Buy Now!</button>
-                                    </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-1"></div>
-                               <div class="card col-md-4 col-md-offset-1"> 
-                                    <div class="header text-center">
-                                        <h2 class="title">Product 1</h2>
-                                    </div>
-                                    <div class="row">
-                                    <div class="col-md-6">
-                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
-                                    </div>
-                                    <div class="col-md-6 product-info">
-                                    <p class="product-brand">by Jack</p>
-                                    <p class="product-desc">a cool product</p>
-                                    <p class="product-price">$100 <span>Rating: 5/5</span></p>
-                                    <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
-                                    <button class="green">Buy Now!</button>
-                                    </div>
-                                    </div>
-                        </div>
-                    </div>  
-                     <div class="row">
-                               <div class="card col-md-4 col-md-offset-1"> 
-                                    <div class="header text-center">
-                                        <h2 class="title">Product 1</h2>
-                                    </div>
-                                    <div class="row">
-                                    <div class="col-md-6">
-                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
-                                    </div>
-                                    <div class="col-md-6 product-info">
-                                    <p class="product-brand">by Jack</p>
-                                    <p class="product-desc">a cool product</p>
-                                    <p class="product-price">$100 <span>Rating: 5/5</span></p>
-                                    <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
-                                    <button class="green">Buy Now!</button>
-                                    </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-1"></div>
-                               <div class="card col-md-4 col-md-offset-1"> 
-                                    <div class="header text-center">
-                                        <h2 class="title">Product 1</h2>
-                                    </div>
-                                    <div class="row">
-                                    <div class="col-md-6">
-                                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
-                                    </div>
-                                    <div class="col-md-6 product-info">
-                                    <p class="product-brand">by Jack</p>
-                                    <p class="product-desc">a cool product</p>
-                                    <p class="product-price">$100 <span>Rating: 5/5</span></p>
-                                    <p class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></p>
-                                    <button class="green">Buy Now!</button>
-                                    </div>
-                                    </div>
-                        </div>
-                    </div>                       
+                @foreach ($products as $product)
+                <section>
+                  <div class="card col-md-4 col-md-offset-1">
+                  <div class="header text-center">
+                    <h2 class="title">{{ $product->name }}</h2>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-6">
+                    <img class="product-image" src="http://www.shmula.com/wp-content/uploads/2007/02/interface-is-product-ux-ui.jpg">
+                  </div>
+                  <div class="col-md-6 product-info">
+                    <p class="product-brand">by {{ $product->brand }}</p>
+                    <p>ID: {{ $product->id }}</p>
+                    <p class="product-desc">{{ $product->description }}</p>
+                    <p class="product-price">Price: {{ $product->price }}</p>
+                    <p class="product-rating">Rating: {{ $product->rating }}/5</p>
+                    <button class="green">Buy Now!</button>
+                  </div>
+                  </div>
+                  <div class="col-md-1"></div>
+                </section>
+              @endforeach
+            @stop                    
                 </div>     
             </div>    
     </section>
-
-  @foreach ($products as $product)
-    <section>
-      <div class="card">
-      <div class="product">
-        <h2>{{ $product->name }}</h2>
-        <p>ID: {{ $product->id }}</p>
-        <p>by {{ $product->brand }}</p>
-        <p>{{ $product->description }}</p>
-        <p>Price: {{ $product->price }}</p>
-        <p>Rating: {{ $product->rating }}/5</p>
-      </div>
-      </div>
-    </section>
-  @endforeach
-@stop
