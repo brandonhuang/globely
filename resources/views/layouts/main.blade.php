63020ca1ff2bbb52f6a3d5b1c025afb5310dcd71
brandonhuang <hi@brandonhuang.io>
2015-11-18 14:33:54 -0800

    globely is bouncy

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
   <script src="https://use.typekit.net/lni5ddu.js"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
+  <script src="/js/popmotion.global.min.js"></script>
   <link rel="stylesheet" type="text/css" href="/css/app.css"></link>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   @yield('javascript')
