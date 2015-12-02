a21c0ae29af0d2054837e1d8e984e73473636732
brandonhuang <hi@brandonhuang.io>
2015-12-02 14:10:05 -0800

    updated navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,15 @@
-@if($currentUser->type == 'distributor')
+@if(!$currentUser)
+  <div class="navbar">
+    <a href="/">
+      <img src="/images/globely_white_logo.png" class="logo" alt="Globley Logo">
+    </a>
+    <nav>
+      <a href="/products">Search</a>
+      <a href="http://localhost:8000/auth/login">Login</a>
+      <a href="/apply" class="with-border">Apply</a>
+    </nav>
+  </div>
+@elseif($currentUser->type == 'distributor')
   <div class="navbar">
     <a href="/">
       <img src="/images/globely_white_logo.png" class="logo" alt="Globley Logo">
@@ -9,27 +20,15 @@
       <a href="/users/{{ $currentUser->id }}">{{ $currentUser->company_name }} <div style="background-image: url(/user_images/{{ $currentUser->id }}.png)" class="nav-img"></div></a>
     </nav>
   </div>
-@elseif($currentUser->type == 'supplier')
+@else($currentUser->type == 'supplier')
   <div class="navbar">
     <a href="/">
       <img src="/images/globely_white_logo.png" class="logo" alt="Globley Logo">
     </a>
     <nav>
-      <a href="/products/create">Add Products</a>
-      <a href="/users">Users</a>
-      <a href="/users/{{ $currentUser->id }}" class="with-border">Profile</a>
+      <a href="/products/create">Add Product</a>
       <a href="/auth/logout">Logout</a>
-    </nav>
-  </div>
-@else
-  <div class="navbar">
-    <a href="/">
-      <img src="/images/globely_white_logo.png" class="logo" alt="Globley Logo">
-    </a>
-    <nav>
-      <a href="/products">Browse</a>
-      <a href="/apply" class="with-border">Apply</a>
-      <a href="http://localhost:8000/auth/login">Login</a>
+      <a href="/users/{{ $currentUser->id }}">{{ $currentUser->company_name }} <div style="background-image: url(/user_images/{{ $currentUser->id }}.png)" class="nav-img"></div></a>
     </nav>
   </div>
 @endif
